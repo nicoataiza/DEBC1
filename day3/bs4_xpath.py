fd2a4302c754d802e94ce9baa9e95cfebadd0c91
nicoataiza <christian.ataiza@obf.ateneo.edu>
2022-06-01 20:57:54 +0800

    changed bs4 sample and added xpath sample

--- day3/bs4_xpath.py
+++ day3/bs4_xpath.py
@@ -1,13 +1,15 @@
 import bs4
+from lxml import etree
 
 
 with open("de-resources-fellows/bs4-codes/index3.html", "r") as html_file:
     content = html_file.read()
 
 soup = bs4.BeautifulSoup(content, 'lxml')
+dom = etree.HTML(str(soup))
 
-tags = soup.select('div.w3-panel > p')
+tags = dom.xpath('//div[@class="w3-panel"]//p')
+
 
 for _tags in tags:
-    print(_tags.text)
-
+    print(_tags)
