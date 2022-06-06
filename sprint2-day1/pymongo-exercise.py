c631f565e01f6309ee13a0fd21dceb4e22ff2f58
nicoataiza <christian.ataiza@obf.ateneo.edu>
2022-06-06 22:18:34 +0800

    added test file to try out pymongo library

---
+++ sprint2-day1/pymongo-exercise.py
@@ -0,0 +1,10 @@
+import pymongo
+myclient = pymongo.MongoClient("mongodb://localhost:27017/")
+print(myclient)
+mydb = myclient["mydatabase"]
+
+collist = mydb.list_collection_names()
+
+if "customers" in collist:
+  print("The collection exists.")
+print(collist)
