470466971cd439e46b20ed80e5cd6c24bc9b4388
Nico Ataiza <65964857+nicoataiza@users.noreply.github.com>
2022-06-06 09:34:44 +0800

    Delete day4-storage/google_storage directory

--- day4-storage/google_storage/list_buckets.py
+++
@@ -1,9 +0,0 @@
-import os
-from google.cloud import storage
-
-os.environ['GOOGLE_APPLICATION_CREDENTIALS'] =  r'vernal-verve-351110-d85c476c21a8.json'
-
-storage_client = storage.Client()
-
-for bucket in storage_client.list_buckets():
-    print(bucket)
